fe40292e98fc745b5ec12ac04b93f07873db417e
CleverParty <shanatmail@gmail.com>
2020-06-07 12:56:45 -0400

    challenge problem 1

---
+++ Kastenlauf.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+x = [int(x) for x in input("Enter multiple value: ").split()] 
+print("List of students: ", x)  # taking multiple inputs 
+
+num = x[0]
+print(num)
+start = x[2]
+end = x[5]
+for i in range(num):
+    for j in range(x[1])
+    
