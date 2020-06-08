7d92a48cc13590ef1723be6b4f2fc8d25b0c2ce3
CleverParty <shanatmail@gmail.com>
2020-06-07 20:15:43 -0400

    solved Kastenlauf( german coding challenge 2018)

--- Kastenlauf.py
+++ Kastenlauf.py
@@ -7,6 +7,16 @@ num = x[0]
 print(num)
 start = x[2]
 end = x[5]
-for i in range(num):
-    for j in range(x[1])
-    
+print("vals being printsed")
+for i in range(0,num):
+    print(x[1])
+    for index in range(0,x[1]):
+        print(index)
+        val_x = abs(x[index + 2] - x[index])
+        val_y = abs(x[index + 3] - x[index +1])
+        manhatten_dist = val_x + val_y
+        print("manhatten distance is : ",manhatten_dist)
+        if(manhatten_dist>=1000): # because for the students to be happy they should satisify the 50 meters for every beer so that comes about to 1000 meter for every crate of 20 beers
+            print("happy")
+        else:
+            print("sad")
