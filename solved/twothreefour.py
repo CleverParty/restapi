f57411ca2d1084b180ccd5e743a4538a033a2673
CleverParty <shanatmail@gmail.com>
2020-06-11 21:37:39 -0400

    [update] functionality

---
+++ solved/twothreefour.py
@@ -0,0 +1,17 @@
+import numpy as np 
+
+Class one:
+    def __init__(){
+        print("one node init")
+    }
+
+Class two:
+    def __init__(){
+        print("two node init")
+    }
+
+Class three:
+    def __init__(){
+        print("three node init")
+    }
+
