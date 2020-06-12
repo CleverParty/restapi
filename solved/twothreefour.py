834fdf91625ac91355a7aa2ff6d77dee1471d066
CleverParty <shanatmail@gmail.com>
2020-06-12 03:54:06 -0400

    [update] made changes to nodes

--- solved/twothreefour.py
+++ solved/twothreefour.py
@@ -1,17 +1,22 @@
 import numpy as np 
 
-Class one:
-    def __init__(){
-        print("one node init")
-    }
+class two:
+    def __init__(self,first=1,second=2):
+        print("the two node init")
+        self.first = first  
+        self.second = second
+    def print(self):
+        print("values stored are : {first} and {second} ".format(first = self.first, second = self.second))
 
-Class two:
-    def __init__(){
-        print("two node init")
-    }
+class three:
+    def __init__(self):
+        print("the three node init")
 
-Class three:
-    def __init__(){
-        print("three node init")
-    }
+class four:
+    def __init__(self):
+        print("the four node init")
+
+
+check=two(1,2)
+check.print()
 
